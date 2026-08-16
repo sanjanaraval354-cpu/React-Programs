f3f21c0da99943ec121cdd01a8348fad25f636ba
Raval Sanjana <sanjanaraval354@gmail.com>
2026-08-16 15:01:10 +0530

    Add files via upload

--- App.jsx
+++ App.jsx
@@ -1,17 +1,28 @@
-import { useState } from "react";
-import Student from "./Component/Student";
+import { useMemo, useState } from "react";
 
 function App() {
+  const [number, setNumber] = useState(5);
   const [count, setCount] = useState(0);
-  console.log("App rendered");
+  const result = useMemo(() => {
+    console.log("calculation executed");
+    let total = 0;
+    for (let i = 0; i < 100000000; i++) {
+      total += number;
+    }
+    return total;
+  }, [number]);
   return (
     <div>
-      <h1>React.memo Example</h1>
+      <h1>useMemo Example</h1>
+<h2>Number: {number}</h2>
+      <button onClick={() => setNumber(number + 1)}>
+        Change Number
+      </button>
       <h2>Counter: {count}</h2>
       <button onClick={() => setCount(count + 1)}>
-        Increase
+        Increase Counter
       </button>
-      <Student name="Raj Mehta" />
+      <h3>Result: {result}</h3>
     </div>
   );
 }
